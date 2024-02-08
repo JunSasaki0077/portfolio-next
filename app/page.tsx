8ac4b325ebc0b1276879a05d859571e4a8460063
Jun Sasaki <ikasas0077@gmail.com>
2024-02-08 22:38:02 +0900

    main部分スタイル変更

--- app/page.tsx
+++ app/page.tsx
@@ -13,22 +13,19 @@ export default async function Home() {
   const posts: Post[] = data.articles.slice(0, 10);
   return (
     <>
-      <h2>Jun</h2>
-      <p>Front End Engineer</p>
-      <div>
-        <h3>
-          <AccountCircleIcon />
-          About
-        </h3>
-        <p></p>
+      <h2 className="text-center text-4xl font-bold m-5">Jun</h2>
+      <p className="text-center mb-10">Front End Engineer</p>
+      <div className="text-center mb-10">
+        <AccountCircleIcon />
+        <h3 className="text-center text-2xl font-bold mb-4">About</h3>
         <p>
           1991年生まれ。東京都出身、東京在住のWebエンジニア。
           <br />
           未経験からWeb制作会社コーダー(マークアップ)を経験し
         </p>
       </div>
-      <div>
-        <h3>Skills</h3>
+      <div className="mb-10">
+        <h3 className="text-center text-2xl font-bold mb-4">Skills</h3>
         <ul className="flex flex-wrap gap-2 justify-center w-full ">
           {Skills.map((item, index) => (
             <li key={index}>
@@ -39,11 +36,10 @@ export default async function Home() {
           ))}
         </ul>
       </div>
-      <div>
-        <h3>Contact</h3>
-        <p>お問い合わせ</p>
-        <p>ご用の方は以下よりご連絡ください。</p>
-        <Button variant="outline">お問い合わせ</Button>
+      <div className="text-center mb-10">
+        <h3 className="text-center text-2xl font-bold mb-4">Contact</h3>
+        <p className="mb-6">ご用の方は以下よりご連絡ください。</p>
+        <Button className="bg-sky-500 shadow-2xl">お問い合わせ</Button>
       </div>
       <div>
         <h3 className="text-center text-2xl font-bold mb-4">Blog</h3>
